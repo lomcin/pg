25eb449bbef8c3c52d0a895d0202dcea185dbef4
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2020-10-28 16:31:38 -0300

    [ADDED] points processing in Bezier, DeCasteljau. [ADDED] null verifications. [IMPROVED] buttons styles. [ADDED] drawing to canvas. [ADDED] onresize and onload effects on canvas. [IMPROVED] machine state for BezierApp.

--- miniproject2/js/main.js
+++ miniproject2/js/main.js
@@ -29,15 +29,6 @@ class Rect {
     }
 }
 
-class BezierCurve {
-    constructor() {
-        this.controlPoints = new Array();
-    }
-    append(point) {
-        this.controlPoints.push(point);
-    }
-}
-
 class DeCasteljau {
     static interpolate(pa, pb, t) {
         return new Point(pa.x*(1-t) + pb.x*t,
@@ -55,27 +46,171 @@ class DeCasteljau {
         }
         return actualLevel;
     }
+    static calcPoints(controlPoints, tvalues) {
+        var points = new Array();
+        for (var i = 0; i < tvalues.length; ++i) {
+            points += DeCasteljau.process(controlPoints, tvalues[i]);
+        }
+        return points;
+    }
+}
+
+class BezierCurve {
+    constructor(controlPoints = null, tvalues = 2) {
+        this.controlPoints = null;
+        this._tvalues = null;
+        this._points = null;
+        this._updated = false;
+    }
+    get tvalues() {
+        return this._tvalues;
+    }
+    set tvalues(val) {
+        if (val < 1) {
+            console.error('Invalid value for tvalues. Must be positive greater than zero.');
+            return null;
+        }
+        this._tvalues = new Array();
+        for (var i = 0; i < val; ++i) {
+            this._tvalues.push(i/(val+1));
+        }
+    }
+    append(point) {
+        if (this.controlPoints == null) this.controlPoints = new Array();
+        this.controlPoints.push(point);
+        this._updated = false;
+    }
+    get points() {
+        if (this._points == null || this._updated == false) {
+            if (this._tvalues != null) {
+                this._points = DeCasteljau.calcPoints(this.controlPoints,this._tvalues);
+                this._updated = true;
+            } else {
+                console.warn("tvalues must be defined.");
+            }
+        }
+        return this._points;
+    }
+    set points(a) {
+
+    }
+
 }
 
 class BezierApp {
     constructor(doc) {
         this.canvas = doc.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.updateButton = doc.getElementById('Update');
         this.createButton = doc.getElementById('CreateCurve');
         this.deleteButton = doc.getElementById('DeleteCurve');
         this.curveList = doc.getElementById('CurveList');
+        this.currentCurve = null;
         this.curves = new Array();
+        this.state = "nothing";
+        doc.body.app = this;
+        doc.body.onresize = function (e) {
+            this.app.resize();
+        };
+        doc.body.onload = function (e) {
+            this.app.resize();
+        };
+        this.prepareButtons();
+    }
+    prepareCreateButton() {
+        this.createButton.parent = this;
+        this.createButton.onclick = function (e) {
+            console.log(this.parent);
+            console.log(this);
+            if (this.parent.state == 'newCurve') {
+                this.parent.state = 'nothing';
+                this.classList.remove('active');
+                this.parent.curves.push(this.parent.currentCurve);
+                this.parent.currentCurve = null;
+            } else {
+                this.parent.state = 'newCurve';
+                this.classList.add('active');
+                this.parent.currentCurve = new BezierCurve();
+            }
+            this.parent.run();
+        }
+    }
+    prepareUpdateButton() {
+        this.updateButton.parent = this;
+        this.updateButton.onclick = function (e) {
+            this.parent.run();
+            console.log('run');
+        }
+    }
+    prepareButtons() {
+        this.prepareCreateButton();
+        this.prepareUpdateButton();
+    }
+    drawCurve(curve) {
+        if (curve == null) return;
+        if (curve.points == null) return;
+
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath();
+        this.ctx.moveTo(curve.points[0].x,curve.points[0].y);
+        for (var i = 1; i < curve.points.length; ++i) {
+            this.ctx.lineTo(curve.points[i].x,curve.points[i].y);
+        }
+        this.ctx.closePath();
+    }
+    drawCurves() {
+        for (var i = 0; i < this.curves.length; ++i) {
+            this.drawCurve(this.curves[i]);
+        }
+        this.drawCurrentCurve();
+    }
+    drawCurrentCurve() {
+        this.drawCurve(this.currentCurve);
+    }
+    draw() {
+        this.ctx.strokeStyle = '#000';
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath();
+        this.ctx.moveTo(500,500);
+        this.ctx.lineTo(600,600);
+        this.ctx.closePath();
+        this.ctx.stroke();
+    }
+    run() {
+        switch(this.state) {
+            case "newCurve":
+                document.body.style.cursor = 'cell';
+                break;
+            case "newControlPoint":
+                document.body.style.cursor = 'crosshair';
+                break;
+            default:
+            case "nothing":
+                document.body.style.cursor = 'default';
+                break;
+        }
+        this.drawCurves();
+        this.draw();
+    }
+    resize() {
+        console.log('resize')
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+        this.run();
     }
 }
 var app = new BezierApp(document);
 // console.log(app.canvas);
 var p = new Point(3,4);
-var pa = new Point(1,3);
-var pb = new Point(3,5);
+var pa = new Point(100,300);
+var pb = new Point(300,500);
 var d = new Dimensions(2,2);
 var r = new Rect(p.x,p.y,d.width,d.height);
 
 var c = new BezierCurve();
-c.append();
+c.append(pa);
+c.append(pb);
+c.tvalues = 10;
 
 var controlPoints = [pa,pb];
 var pab = DeCasteljau.interpolate(pa,pb,0.1);
@@ -83,3 +218,7 @@ console.log(pab);
 
 var pab2 = DeCasteljau.process(controlPoints,0.1);
 console.log(pab2);
+
+app.curves.push(c);
+
+app.run();
