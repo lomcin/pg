c558b2e59d5fec2b2be68385519cb3495689c974
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2020-12-29 00:07:45 -0300

    [UPDATED] basic sample shader definition.

--- webgl/0_basic/flatShader.js
+++ webgl/0_basic/flatShader.js
@@ -1,20 +1,20 @@
 class FlatShader extends Shader {
     constructor (gl) {
         
-        var vertex = [
-            'attribute vec2 position;',
-            'void main() {',
-                'gl_Position = vec4(position, 0.0, 1.0);',
-            '}'
-        ].join('\n');
+        var vertex = `
+            attribute vec2 position;
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        `;
 
-        var fragment = [
-            'precision highp float;',
-            'uniform vec4 color;',
-            'void main() {',
-                'gl_FragColor = color;',
-            '}'
-        ].join('\n');
+        var fragment = `
+            precision highp float;
+            uniform vec4 color;
+            void main() {
+                gl_FragColor = color;
+            }
+        `;
 
         super(gl, vertex, fragment);
     }
